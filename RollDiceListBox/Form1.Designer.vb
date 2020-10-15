4d2e4d6c61b9d9d8436d68f35683c3e4ac99da33
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-10-15 08:30:40 -0600

    Add project files.

---
+++ RollDiceListBox/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DiceForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RollButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.DisplayListBox = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'RollButton
+        '
+        Me.RollButton.Location = New System.Drawing.Point(247, 681)
+        Me.RollButton.Name = "RollButton"
+        Me.RollButton.Size = New System.Drawing.Size(235, 127)
+        Me.RollButton.TabIndex = 0
+        Me.RollButton.Text = "&Roll"
+        Me.RollButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClearButton.Location = New System.Drawing.Point(525, 681)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(235, 127)
+        Me.ClearButton.TabIndex = 1
+        Me.ClearButton.Text = "&Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ExitButton.Location = New System.Drawing.Point(798, 681)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(235, 127)
+        Me.ExitButton.TabIndex = 2
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'DisplayListBox
+        '
+        Me.DisplayListBox.FormattingEnabled = True
+        Me.DisplayListBox.ItemHeight = 25
+        Me.DisplayListBox.Location = New System.Drawing.Point(31, 30)
+        Me.DisplayListBox.Name = "DisplayListBox"
+        Me.DisplayListBox.Size = New System.Drawing.Size(1279, 504)
+        Me.DisplayListBox.TabIndex = 0
+        Me.DisplayListBox.TabStop = False
+        '
+        'DiceForm
+        '
+        Me.AcceptButton = Me.RollButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.ExitButton
+        Me.ClientSize = New System.Drawing.Size(1346, 956)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.RollButton)
+        Me.Controls.Add(Me.DisplayListBox)
+        Me.Name = "DiceForm"
+        Me.Text = "Dice role Form"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents RollButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents DisplayListBox As ListBox
+End Class
---
+++ RollDiceListBox/Form1.vb
@@ -0,0 +1,74 @@
+﻿'Luis Torres
+'RCET0265
+'Fall 2020
+'Roll of the Dice 
+'https://github.com/LuisTorres0419/RollDiceListBox.git
+
+Public Class DiceForm
+
+    'soeahytasnrytrfty
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        DisplayListBox.Items.Clear()
+    End Sub
+
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+
+        Randomize()
+
+        Dim randomNumber As Integer
+        Dim lable, line1, line2, dash As String
+        Dim data(10) As Integer
+
+        DisplayListBox.Items.Add("                                                                     
+Roll of the Dice")
+
+        For i = 1 To 1000
+            randomNumber = CInt(getRandomNumber(1, 6))
+            data(randomNumber - 2) += 1
+        Next
+
+
+
+        For i = 2 To 12
+            line1 = line1 & String.Format("{0,11}", i) & "|"
+        Next
+
+        DisplayListBox.Items.Add(line1)
+        dash = (StrDup(177, "-"))
+        DisplayListBox.Items.Add(dash)
+
+
+        For i = 0 To 10
+            line2 = line2 & String.Format("{0,10}", data(i)) & "|"
+        Next
+
+        DisplayListBox.Items.Add(line2)
+        DisplayListBox.Items.Add(dash)
+        DisplayListBox.Items.Add("")
+        DisplayListBox.Items.Add("")
+
+    End Sub
+
+    Function getRandomNumber(ByVal minimum As Integer, ByVal maximum As Integer) As Integer
+        Dim value1, value2 As Single
+        Dim amount As Integer
+
+        Do
+
+            value1 = (maximum * Rnd()) + 0.5
+            value2 = (maximum * Rnd()) + 0.5
+
+        Loop While value1 < minimum - 0.5 Or value1 > maximum + 0.5 Or
+ value2 < minimum - 0.5 Or value2 > maximum + 0.5
+
+        amount = CInt(value1) + CInt(value2)
+
+        Return amount
+    End Function
+
+End Class
